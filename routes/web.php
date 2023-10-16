98ce203ad38a46fef307858a014d9d4785f3b888
BagusFary <bagusfaryananta@gmail.com>
2023-10-16 11:32:51 +0700

    Third Commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -28,6 +31,6 @@ use Illuminate\Support\Facades\Route;
 // });
 
 
-Route::get('/', [PageController::class, 'index']);
-Route::get('/about', [PageController::class, 'about']);
-Route::get('/article/{id}', [PageController::class, 'articles']);
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [AboutController::class, 'index']);
+Route::get('/article/{id}', [ArticleController::class, 'index']);
